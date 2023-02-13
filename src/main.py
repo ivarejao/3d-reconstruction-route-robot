5238e1427317693f2988ff83e447b101dcda47e8
OtavioCozer <otaviocozer22@gmail.com>
2023-02-13 18:36:19 -0300

    Matriz a ja criada. Porem unidades das Matrizes P0, P1, P2 e P3 estao erradas.

--- src/main.py
+++ src/main.py
@@ -6,23 +6,39 @@ from parametros import P0, P1, P2, P3
 from video import VideoReader
 
 
-def MyDetectMarkers(f: np.ndarray) -> np.ndarray:
-    corner = []
+def findCenterPixelAruco(f: np.ndarray) -> np.ndarray:
+    corner_ = []
     corners_, ids_, _ = arucoDetecter.detectMarkers(f)
     if ids_ is None:
-        return np.array(corner, dtype="float32")
+        return np.array(corner_, dtype="float32")
 
     ids_ = ids_.reshape(-1)
     if ids_[0] == 0:
-        corner = corners_[0].reshape(-1, 2)
-    return np.array(corner, dtype="float32")
+        corner_ = corners_[0].reshape(-1, 2)
+        xc = (corner_[0][0] + corner_[1][0] + corner_[2][0] + corner_[3][0]) / 4
+        yc = (corner_[0][1] + corner_[1][1] + corner_[2][1] + corner_[3][1]) / 4
+        corner_ = [xc, yc, 1]  # corner na verdade eh o centro
+    return np.array(corner_, dtype="float32")
 
 
-def createM(ps: np.ndarray, xs: np.ndarray) -> np.ndarray:
-    # if ps.shape != (-1, 3, 4) or xs.shape != (-1, 3):
-    #     raise Exception
-    l = np.shape[0] * 3
-    M = np.zeros((l, 6))
+xp = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+print(xp[1:2, :])
+
+
+def createM(ps_: np.ndarray, xs: np.ndarray) -> np.ndarray:
+    npontos = xs.shape[0]
+    if npontos < 2:
+        return np.array([], dtype="float32")
+
+    l, c = npontos * 3, 4 + npontos
+    M = np.zeros((l, c))
+
+    for p, x, j in zip(ps_, xs, range(0, npontos)):
+        li, lo = 3*j, 3*j + 3
+        ci, co = 4 + j, 4 + j + 1
+        M[li:lo, 0:4] = p
+        M[li:lo, ci:co] = -x.reshape(3, -1)
+    return np.array([], dtype="float32")
 
 
 v0, v1 = VideoReader("../data/videos/camera-00.mp4"), VideoReader("../data/videos/camera-01.mp4")
@@ -30,6 +46,7 @@ v2, v3 = VideoReader("../data/videos/camera-02.mp4"), VideoReader("../data/video
 
 dictionary = aruco.getPredefinedDictionary(aruco.DICT_4X4_50)
 arucoDetecter = aruco.ArucoDetector(dictionary)
+pdict = [P0, P1, P2, P3]
 
 for f0, f1, f2, f3 in zip(v0, v1, v2, v3):
     # RETIRAR DEPOIS
@@ -44,16 +61,17 @@ for f0, f1, f2, f3 in zip(v0, v1, v2, v3):
     frame = aruco.drawDetectedMarkers(frame, corners, ids)
     #######################################################
 
-    corner0 = MyDetectMarkers(f0)
-    corner1 = MyDetectMarkers(f1)
-    corner2 = MyDetectMarkers(f2)
-    corner3 = MyDetectMarkers(f3)
+    centers = [findCenterPixelAruco(f0), findCenterPixelAruco(f1), findCenterPixelAruco(f2), findCenterPixelAruco(f3)]
+    cs = []
+    ps = []
+    for i, center in enumerate(centers):
+        if len(center) > 0:
+            cs.append(center)
+            ps.append(pdict[i])
 
-    print(f"corner0:\n {corner0}")
-    print(f"corner1:\n {corner1}")
-    print(f"corner2:\n {corner2}")
-    print(f"corner3:\n {corner3}")
-    print(f"###############################")
+    cs = np.array(cs, dtype="float32")
+    ps = np.array(ps, dtype="float32")
+    createM(ps, cs)
 
     cv2.imshow("Video", frame)
     if cv2.waitKey(5) == ord('q'):
--- src/parametros.py
+++ src/parametros.py
@@ -9,11 +9,11 @@ def camera_parameters(file):
     K = np.array(camera_data['intrinsic']['doubles']).reshape(3, 3)
     res = [camera_data['resolution']['width'],
            camera_data['resolution']['height']]
-    tf = np.array(camera_data['extrinsic']['tf']['doubles']).reshape(4, 4)
+    tf = np.array(camera_data['extrinsic']['tf']['doubles']).reshape(4, 4) #converte referencial da camera para o mundo
     R = tf[:3, :3]
     T = tf[:3, 3].reshape(3, 1)
     dis = np.array(camera_data['distortion']['doubles'])
-    return K, R, T, res, dis, tf
+    return K, R, T, res, dis, np.linalg.inv(tf) # np.linalg.inv(tf) converte referencial do mundo para a camera
 
 
 # Load cameras parameters
