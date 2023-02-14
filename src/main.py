d3894813845e8fa6394d74831def9eddbd0f717f
OtavioCozer <otaviocozer22@gmail.com>
2023-02-14 19:29:31 -0300

    Svd e plot E REFORMAT

--- src/main.py
+++ src/main.py
@@ -1,55 +1,23 @@
 import cv2
 import cv2.aruco as aruco
+import matplotlib.pyplot as plt
 import numpy as np
-from parametros import P0, P1, P2, P3
 
+from parametros import P0, P1, P2, P3
 from video import VideoReader
 
 
-def findCenterPixelAruco(f: np.ndarray) -> np.ndarray:
-    corner_ = []
-    corners_, ids_, _ = arucoDetecter.detectMarkers(f)
-    if ids_ is None:
-        return np.array(corner_, dtype="float32")
+def presentImage(centers, *args):
+    f0, f1, f2, f3 = args
+    if len(centers[0]) > 0:
+        f0 = cv2.circle(f0, centers[0][0:2].astype(int), 10, (255, 0, 0), -1)
+    if len(centers[1]) > 0:
+        f1 = cv2.circle(f1, centers[1][0:2].astype(int), 10, (255, 0, 0), -1)
+    if len(centers[2]) > 0:
+        f2 = cv2.circle(f2, centers[2][0:2].astype(int), 10, (255, 0, 0), -1)
+    if len(centers[3]) > 0:
+        f3 = cv2.circle(f3, centers[3][0:2].astype(int), 10, (255, 0, 0), -1)
 
-    ids_ = ids_.reshape(-1)
-    if ids_[0] == 0:
-        corner_ = corners_[0].reshape(-1, 2)
-        xc = (corner_[0][0] + corner_[1][0] + corner_[2][0] + corner_[3][0]) / 4
-        yc = (corner_[0][1] + corner_[1][1] + corner_[2][1] + corner_[3][1]) / 4
-        corner_ = [xc, yc, 1]  # corner na verdade eh o centro
-    return np.array(corner_, dtype="float32")
-
-
-xp = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
-print(xp[1:2, :])
-
-
-def createM(ps_: np.ndarray, xs: np.ndarray) -> np.ndarray:
-    npontos = xs.shape[0]
-    if npontos < 2:
-        return np.array([], dtype="float32")
-
-    l, c = npontos * 3, 4 + npontos
-    M = np.zeros((l, c))
-
-    for p, x, j in zip(ps_, xs, range(0, npontos)):
-        li, lo = 3*j, 3*j + 3
-        ci, co = 4 + j, 4 + j + 1
-        M[li:lo, 0:4] = p
-        M[li:lo, ci:co] = -x.reshape(3, -1)
-    return np.array([], dtype="float32")
-
-
-v0, v1 = VideoReader("../data/videos/camera-00.mp4"), VideoReader("../data/videos/camera-01.mp4")
-v2, v3 = VideoReader("../data/videos/camera-02.mp4"), VideoReader("../data/videos/camera-03.mp4")
-
-dictionary = aruco.getPredefinedDictionary(aruco.DICT_4X4_50)
-arucoDetecter = aruco.ArucoDetector(dictionary)
-pdict = [P0, P1, P2, P3]
-
-for f0, f1, f2, f3 in zip(v0, v1, v2, v3):
-    # RETIRAR DEPOIS
     f0_ = cv2.resize(f0, (644, 364))
     f1_ = cv2.resize(f1, (644, 364))
     f2_ = cv2.resize(f2, (644, 364))
@@ -57,24 +25,94 @@ for f0, f1, f2, f3 in zip(v0, v1, v2, v3):
     f01 = cv2.hconcat([f0_, f1_])
     f23 = cv2.hconcat([f2_, f3_])
     frame = cv2.vconcat([f01, f23])
-    corners, ids, _ = arucoDetecter.detectMarkers(frame)
-    frame = aruco.drawDetectedMarkers(frame, corners, ids)
-    #######################################################
-
-    centers = [findCenterPixelAruco(f0), findCenterPixelAruco(f1), findCenterPixelAruco(f2), findCenterPixelAruco(f3)]
-    cs = []
-    ps = []
-    for i, center in enumerate(centers):
-        if len(center) > 0:
-            cs.append(center)
-            ps.append(pdict[i])
-
-    cs = np.array(cs, dtype="float32")
-    ps = np.array(ps, dtype="float32")
-    createM(ps, cs)
 
     cv2.imshow("Video", frame)
     if cv2.waitKey(5) == ord('q'):
-        break
+        return False
 
-cv2.destroyAllWindows()
+    return True
+
+
+def findCenterPixelAruco(f: np.ndarray, arucoDetecter) -> np.ndarray:
+    corner = []
+    corners, ids, _ = arucoDetecter.detectMarkers(f)
+    if ids is None:
+        return np.array(corner, dtype="float32")
+
+    ids = ids.reshape(-1)
+    if ids[0] == 0:
+        corner = corners[0].reshape(-1, 2)
+        xc = (corner[0][0] + corner[1][0] + corner[2][0] + corner[3][0]) / 4
+        yc = (corner[0][1] + corner[1][1] + corner[2][1] + corner[3][1]) / 4
+        corner = [xc, yc, 1]  # corner na verdade eh o centro
+    return np.array(corner, dtype="float32")
+
+
+def createM(ps: np.ndarray, xs: np.ndarray) -> np.ndarray:
+    npontos = xs.shape[0]
+    if npontos < 2:
+        return np.array([], dtype="float32")
+
+    l, c = npontos * 3, 4 + npontos
+    M = np.zeros((l, c))
+
+    for p, x, j in zip(ps, xs, range(0, npontos)):
+        li, lo = 3 * j, 3 * j + 3
+        ci, co = 4 + j, 4 + j + 1
+        M[li:lo, 0:4] = p
+        M[li:lo, ci:co] = -x.reshape(3, -1)
+    return np.array(M, dtype="float32")
+
+
+def main():
+    v0, v1 = VideoReader("../data/videos/camera-00.mp4"), VideoReader("../data/videos/camera-01.mp4")
+    v2, v3 = VideoReader("../data/videos/camera-02.mp4"), VideoReader("../data/videos/camera-03.mp4")
+
+    cv2.namedWindow('Video', cv2.WINDOW_AUTOSIZE | cv2.WINDOW_KEEPRATIO | cv2.WINDOW_GUI_NORMAL)
+
+    dictionary = aruco.getPredefinedDictionary(aruco.DICT_4X4_50)
+    arucoDetecter = aruco.ArucoDetector(dictionary)
+    pdict = [P0, P1, P2, P3]
+    arucoPositions = []
+
+    for f0, f1, f2, f3 in zip(v0, v1, v2, v3):
+        centers = [findCenterPixelAruco(f0, arucoDetecter), findCenterPixelAruco(f1, arucoDetecter),
+                   findCenterPixelAruco(f2, arucoDetecter), findCenterPixelAruco(f3, arucoDetecter)
+                   ]
+
+        if not presentImage(centers, f0, f1, f2, f3):
+            break
+
+        cs = []
+        ps = []
+        for i, center in enumerate(centers):
+            if len(center) > 0:
+                cs.append(center)
+                ps.append(pdict[i])
+
+        cs = np.array(cs, dtype="float32")
+        ps = np.array(ps, dtype="float32")
+        m = createM(ps, cs)
+        u, s, vh = np.linalg.svd(m)
+        x, y, z, w = np.array(vh[-1, 0:4]).T  # aruco position
+        arucoPositions.append(np.array([x / w, y / w, z / w]))
+
+    cv2.destroyAllWindows()
+
+    arucoPositions = np.array(arucoPositions)
+    fig = plt.figure()
+    ax = fig.add_subplot(projection='3d')
+    ax.scatter(arucoPositions[:, 0], arucoPositions[:, 1], arucoPositions[:, 2])
+    ax.set_xlabel('X')
+    ax.set_ylabel('Y')
+    ax.set_zlabel('Z')
+    ax.set_xlim(-2.0, 2.0)
+    ax.set_ylim(-1.0, 1.0)
+    ax.set_zlim(0, 1.0)
+    plt.show()
+
+    cv2.destroyAllWindows()
+
+
+if __name__ == "__main__":
+    main()
